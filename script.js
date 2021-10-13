af776e21aa9ea20886a46cbd6de5c90195f76862
Akshay Kumar Akkinapelli <64968793+ak-akkinapelli@users.noreply.github.com>
2021-10-14 01:22:16 +0530

    V1 of tic tac toe

---
+++ script.js
@@ -0,0 +1,98 @@
+/*Initial Declaration */
+
+const Status = document.querySelector('.game--status');
+let Active = true;
+let currentPlayer = 'X';
+let gameState = ["","","","","","","","",""];
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const playerTurn = () => `It's ${currentPlayer}'s turn`;
+const DrawMessage = () => `Draw Match`;
+const WinningMessage =() => `Player ${currentPlayer} has won.`
+
+Status.innerHTML = playerTurn();
+
+/*Handle cell click*/
+function cellClick(clickedCellEvent){
+ const clicked = clickedCellEvent.target;
+ const clickedCellIndex = parseInt(clicked.getAttribute(`data-cell-index`));
+
+if(gameState[clickedCellIndex] !== "" || !Active)
+{
+    return;
+}
+
+cellPlayed(clicked, clickedCellIndex);
+resultValidation();
+}
+
+/*Add player symbol to UI and updating status*/
+function cellPlayed(clicked, clickedCellIndex)
+{
+gameState[clickedCellIndex] = currentPlayer;
+clicked.innerHTML = currentPlayer;
+}
+
+/*check if win/draw*/
+function resultValidation()
+{
+    let won = false;
+ for(let i =0;i<=7;i++)
+ {
+     const winningCondition = winningConditions[i]; 
+     let a= gameState[winningCondition[0]];
+     let b= gameState[winningCondition[1]];
+     let c= gameState[winningCondition[2]];
+     if(a ==="" || b ===""||c ==="")
+     {
+     continue;
+     }
+     if(a=== b && b ===c)
+     {
+       won = true;
+       break;
+     }
+    }
+     if(won)
+     {
+        Status.innerHTML = WinningMessage();
+        Active = false;
+        return;
+     }
+    
+    if(!gameState.includes(""))
+    {
+        Status.innerHTML = DrawMessage();
+        Active= false;
+        return;
+    }
+    PlayerChange();
+ 
+}
+
+/*change current player */
+function PlayerChange() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    Status.innerHTML = playerTurn();
+}
+
+/*restart */
+function GameRestart() {
+    Active = true;
+    currentPlayer = 'X';
+    gameState=["","","","","","","","",""];
+    Status.innerHTML = playerTurn();
+    document.querySelectorAll('.cell').forEach(cell=> cell.innerHTML = "");
+
+}
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClick));
+document.querySelector('.restart').addEventListener('click',GameRestart);
